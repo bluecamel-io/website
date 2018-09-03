4bd1ea1432a3c62977a01a0a7b70cd65fa0e83a6
Andrew Ross <andrew.ross.mail@gmail.com>
2018-09-03 15:10:06 -0400

    make page templates inherit from base.html

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Dockerized Flask Landing Page'
+    return render_template('index.html', title='Home')
 
 
 @app.route('/ecommerce')
 def ecommerce():
-    return 'Dockerized Flask E-Commerce Page'
+    return render_template('ecommerce.html', title='E-commerce')
 
 
 @app.route('/healthcare')
 def healthcare():
-    return 'Dockerized Flask Healthcare Page'
+    return render_template('healthcare.html', title='Healthcare')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/contact')
+def contace():
+    return render_template('contact.html', title='Contact')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT'))
