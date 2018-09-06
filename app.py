318eefd4287dc47c246a38baea7d62e8cc35ab47
Alec Ross <alecross89@gmail.com>
2018-09-06 14:54:06 -0400

    added multiple emails

--- app.py
+++ app.py
@@ -34,13 +34,14 @@ def contact():
     
     if request.method == 'POST':
         sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+        # sg = sendgrid.SendGridAPIClient(apikey=)
         form = ContactForm()
 
         #email user provideds --- for testing, put your email into the form
         client_email = request.form.get('email')
 
         #one of our emails to receive clients email --- change to your email if you wanna test it yourself
-        one_of_our_emails = "alecross89@gmail.com"
+        one_of_our_emails = ["alecross89@gmail.com", "andrew.ross.mail@gmail.com"]
         
         # getting the message and subject of the email
         content_of_email = request.form.get('message')
@@ -52,14 +53,16 @@ def contact():
         # passing message of email to Content class
         content = Content("text/plain", content_of_email)
         
-        # passing our email to Email class
-        to_email = Email(one_of_our_emails)
-    
-        mail = Mail(from_email, email_subject, to_email, content)
-        response = sg.client.mail.send.post(request_body=mail.get())
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        for i in range(len(one_of_our_emails)):
+
+            # passing our email to Email class
+            to_email = Email(one_of_our_emails[i])
+        
+            mail = Mail(from_email, email_subject, to_email, content)
+            response = sg.client.mail.send.post(request_body=mail.get())
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
         return 'form submitted'
     else:
         form = ContactForm()
@@ -70,4 +73,3 @@ def contact():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT'))
-
